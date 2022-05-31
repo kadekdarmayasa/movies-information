0d3c1972bafd77fbcdca6825830d2dbdc39a849f
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-05-31 21:59:44 +0800

    Adding page features

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 (() => {
+	let page = 1;
+
 	const timeout = function () {
 		return new Promise((resolve, reject) => {
 			reject();
@@ -15,6 +17,7 @@
 
 	const reset = function () {
 		document.getElementById('card-container').innerHTML = '';
+		document.querySelector('.prev-next').innerHTML = '';
 	};
 
 	const createCard = function (movie) {
@@ -31,7 +34,7 @@
 								</div>`;
 	};
 
-	const getDetails = function (movieDetail) {
+	const renderDetails = function (movieDetail) {
 		return `
 						<div class="modal-header">
 								<h3 class="fw-semibold modal-title" id="movieDetailLabel">${movieDetail.Title}</h3>
@@ -88,57 +91,150 @@
 					`;
 	};
 
-	const showContent = function (results) {
-		const movies = results.Search;
-		let cards = '';
-		if (movies !== undefined) {
-			for (const movie of movies) {
-				if (movie.Poster !== 'N/A') {
-					cards += createCard(movie);
-					document.getElementById('card-container').innerHTML = cards;
-				}
-			}
-		} else {
-			const h5 = document.createElement('h5');
-			h5.className = 'text-center fw-semibold';
-			h5.innerText = "Movie doesn't exist";
-
-			const p = document.createElement('p');
-			p.innerText = "Please input another keyword which approach to movie's name";
-			p.className = 'text-center text-muted';
-
-			const cardContainer = document.getElementById('card-container');
-			cardContainer.appendChild(h5);
-			cardContainer.appendChild(p);
-		}
-
-		const showDetails = document.querySelectorAll('.showDetails');
-
-		showDetails.forEach((showDetail) => {
-			showDetail.addEventListener('click', function () {
-				let id = this.dataset.imdbid;
-				fetch(`http://www.omdbapi.com/?apikey=36a96d2e&i=${id}`)
-					.then((response) => response.json())
-					.then((movieDetail) => {
-						document.querySelector('.modal-content').innerHTML = getDetails(movieDetail);
-					});
+	const updateDetails = function (id) {
+		fetch('http://www.omdbapi.com/?apikey=36a96d2e&i=' + id)
+			.then((response) => response.json())
+			.then((movieDetail) => {
+				document.querySelector('.modal-content').innerHTML = renderDetails(movieDetail);
 			});
+	};
+
+	const responNotFound = function (keyword) {
+		page = 1;
+
+		reset();
+
+		Promise.race([
+			fetch('http://www.omdbapi.com/?apikey=36a96d2e&s=' + keyword + '&page=' + page)
+				.then((response) => ({
+					keyword: keyword,
+					response: response.json(),
+				}))
+				.then(renderMovies)
+				.then(hideLoadingIndicator),
+			timeout(),
+		]).catch(showLoadingIndicator);
+	};
+
+	const renderMovies = function (results) {
+		results.response.then((result) => {
+			let movies = result.Search;
+			let cards = '';
+
+			if (movies !== undefined) {
+				for (const movie of movies) {
+					if (movie.Poster !== 'N/A') {
+						cards += createCard(movie);
+						document.getElementById('card-container').innerHTML = cards;
+						let prevNextButton = document.querySelector('.prev-next');
+
+						if (results.pageNumber > 1) {
+							prevNextButton.innerHTML = `
+									<nav aria-label="Page navigation example">
+										<ul class="pagination d-flex justify-content-center"> 
+											<li class="page-item me-4">
+												<a class="prev btn btn-primary" type="button" data-keyword="${results.keyword}">Previous</a>
+											</li>
+											<li class="page-item">
+												<a class="next btn btn-primary" type="button" data-keyword="${results.keyword}">Next</a>
+											</li>
+										</ul>
+									</nav>`;
+						} else {
+							prevNextButton.innerHTML = `
+								<nav aria-label="Page navigation example">
+									<ul class="pagination d-flex justify-content-center">	
+										<li class="page-item">
+											<a class="next btn btn-primary" type="button" data-keyword="${results.keyword}">Next</a>
+										</li>
+									</ul>
+							</nav>`;
+						}
+					}
+				}
+			} else {
+				if (results.page) {
+					responNotFound(results.keyword);
+					return;
+				}
+
+				const cardContainer = document.getElementById('card-container');
+				cardContainer.innerHTML = `
+					<h5 class="text-center fw-semibold not-found-movies">Movie doesn't exist</h5>
+					<p class="text-center text-muted">Please input another keyword which approach to movie's name</p>
+				`;
+			}
 		});
 	};
 
+	document.addEventListener('click', function (e) {
+		if (e.target.classList.contains('showDetails')) {
+			let id = e.target.dataset.imdbid;
+			updateDetails(id);
+		}
+
+		if (e.target.classList.contains('prev')) {
+			let searchValue = e.target.dataset.keyword;
+			if (page > 1) {
+				document.querySelector('.prev').style.display = 'flex';
+				--page;
+			}
+			fetch('http://www.omdbapi.com/?apikey=36a96d2e&s=' + searchValue + '&page=' + page)
+				.then((response) => ({
+					keyword: searchValue,
+					pageNumber: page,
+					page: true,
+					response: response.json(),
+				}))
+				.then(renderMovies);
+		}
+
+		if (e.target.classList.contains('next')) {
+			let searchValue = e.target.dataset.keyword;
+			++page;
+
+			reset();
+
+			Promise.race([
+				fetch('http://www.omdbapi.com/?apikey=36a96d2e&s=' + searchValue + '&page=' + page)
+					.then((response) => ({
+						keyword: searchValue,
+						pageNumber: page,
+						page: true,
+						response: response.json(),
+					}))
+					.then(renderMovies)
+					.then(hideLoadingIndicator),
+				timeout(),
+			]).catch(showLoadingIndicator);
+		}
+	});
+
+	// Trigger if user press enter
+	document.querySelector('.searchValue').addEventListener('keypress', function (event) {
+		if (event.key == 'Enter') {
+			event.preventDefault();
+			document.querySelector('.search').click();
+		}
+	});
+
+	// Trigger if user click search button
 	document.querySelector('.search').addEventListener('click', function () {
 		let searchValue = document.querySelector('input.searchValue').value;
 
 		reset();
 
-		(async () => {
-			Promise.race([
-				fetch('http://www.omdbapi.com/?apikey=36a96d2e&s=' + searchValue)
-					.then((response) => response.json())
-					.then(showContent)
-					.then(hideLoadingIndicator),
-				timeout(),
-			]).catch(showLoadingIndicator);
-		})();
+		Promise.race([
+			fetch('http://www.omdbapi.com/?apikey=36a96d2e&s=' + searchValue)
+				.then((response) => ({
+					keyword: searchValue,
+					pageNumber: 1,
+					page: false,
+					response: response.json(),
+				}))
+				.then(renderMovies)
+				.then(hideLoadingIndicator),
+			timeout(),
+		]).catch(showLoadingIndicator);
 	});
 })();
